507e47867de6217674dd4992f0b7c74367d60cba
turboc1208 <chip.cox@coxdogs.com>
2017-07-24 13:24:47 -0500

    new server

--- spotcontrol.py
+++ spotcontrol.py
@@ -7,8 +7,8 @@ class spotcontrol(appapi.my_appapi):
   def initialize(self):
     self.LOGLEVEL="DEBUG"
     self.log("SpotControl App")
-    self.run_daily(self.timer_handler_on,datetime.time(4,0,0))
-    self.run_daily(self.timer_handler_off,datetime.time(5,30,0))    
+    self.run_daily(self.timer_handler_on,datetime.time(23,30,0))
+    self.run_daily(self.timer_handler_off,datetime.time(1,00,0))    
 
   def timer_handler_on(self,kwargs):
     self.process_event("on")
@@ -17,23 +17,7 @@ class spotcontrol(appapi.my_appapi):
     self.process_event("off")
 
   def process_event(self,state):
-    light_list=[]
-    light_list=self.build_light_list("group.spots_lights")
-    self.log("light_list={}".format(light_list))
     if state=="on":
-      for light in light_list:
-        self.turn_on(light)
+      self.turn_on("input_boolean.spot")
     else:
-      for light in light_list:
-        self.turn_off(light)
- 
-  def build_light_list(self,groupin):
-    result=[]
-    devtype, devname = self.split_entity(groupin)
-    if devtype=="group":
-      for entity in self.get_state(groupin,attribute="all")["attributes"]["entity_id"]:
-        result.append(self.build_light_list(entity))
-    else:
-      return groupin
-    return result
-  
+      self.turn_off("input_boolean.spot")
