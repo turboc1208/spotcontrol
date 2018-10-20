46632a2a1dd30bff2273291796def3155a41136b
turboc1208 <chip.cox@coxdogs.com>
2018-10-20 09:38:45 -0500

    initial commit

--- spotcontrol.py
+++ spotcontrol.py
@@ -1,29 +1,24 @@
-import my_appapi as appapi
-import inspect
+import appdaemon.plugins.hass.hassapi as hass
 import datetime
+import time
+#import my_appapi as appapi
 
-class spotcontrol(appapi.my_appapi):
-
+#class spotcontrol(appapi.my_appapi):
+class spotcontrol(hass.Hass):
   def initialize(self):
     self.LOGLEVEL="DEBUG"
     self.log("SpotControl App")
-    self.run_daily(self.timer_handler_on,datetime.time(23,30,0))
-    self.run_daily(self.timer_handler_off,datetime.time(1,0,0))    
+    self.run_in(self.timer_handler_on,3*60,target="lights.office_lights")
+    self.print_schedule()
+    self.log("initialization complete");
+  def print_schedule(self):
+    schedule=self.get_scheduler_entries()
+    for app in schedule:
+      self.log("schedule_entities={}".format(app))
+      for handle in schedule[app]:
+        self.log("{} - handle={}-{}".format(type(handle),handle,schedule[app][handle]))
+        for attribute in  schedule[app][handle]:
+          self.log("{}-{}".format(attribute,schedule[app][handle][attribute]))
 
   def timer_handler_on(self,kwargs):
-    self.process_event("on")
-
-  def timer_handler_off(self,kwargs):
-    self.process_event("off")
-
-  def process_event(self,state):
-    if state=="on":
-      self.turn_on("input_boolean.spot")  # turn on override group first
-      for entity in self.build_entity_list("group.app_spotcontrol_spots_lights",["light","switch","fan"]):
-        self.log("turning on {}".format(entity))
-        self.turn_on(entity)
-    else:
-      self.turn_off("input_boolean.spot")
-      for entity in self.build_entity_list("group.app_spotcontrol_spots_lights",["light","switch","fan"]):
-        self.log("turning off {}".format(entity))
-        self.turn_off(entity)
+    self.print_schedule()    
